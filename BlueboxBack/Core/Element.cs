e3409f6e654bba7a51cb5b07cace8931ecadb1f0
Dmitriy Balykin <b_dmitriy82@mail.ru>
2015-01-28 15:38:12 +0200

    Refactor Element class

--- BlueboxBack/Core/Element.cs
+++ BlueboxBack/Core/Element.cs
@@ -4,10 +4,25 @@ using System.Text;
 
 namespace BlueboxBack.Core
 {
-    public enum Element
+    public class Element
     {
-        Undefined,
-        Filled,
-        Cleared
+        enum ElementType
+        {
+            Undefined,
+            Filled,
+            Cleared
+        }
+        public bool Highlighted { get; private set; }
+        public ElementType Type { get; private set; }
+        public Element(Element el, bool highlighted)
+        {
+            this.Type = el.Type;
+            this.Highlighted = highlighted;
+        }
+        public Element(ElementType type, bool highlighted)
+        {
+            this.Type = type;
+            this.Highlighted = highlighted;
+        }
     }
 }
