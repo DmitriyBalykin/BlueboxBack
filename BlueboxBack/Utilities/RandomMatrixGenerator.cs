fad44face16475103e8f45b82328f4abdb5e8b20
DmitriyBalykin <b_dmitriy82@mail.ru>
2015-01-28 23:20:02 +0200

    Rework Random solution generator

--- BlueboxBack/Utilities/RandomMatrixGenerator.cs
+++ BlueboxBack/Utilities/RandomMatrixGenerator.cs
@@ -7,30 +7,54 @@ namespace BlueboxBack.Utilities
 {
     class RandomMatrixGenerator
     {
-        public static DataMatrix GetRandomMatrix(short width, short height)
+        private const int BASIC_DENSITY = 51;
+        private const int SPACE_DENSITY = 5;
+        private const int GROUP_DENSITY = 95;
+
+        private const int LOW_THRESHOLD = 0;
+        private const int HIGH_THRESHOLD = 100;
+
+        private const int DENSITY_CHANGE_FACTOR = 3;
+
+        private static int space_density = SPACE_DENSITY;
+        private static int group_density = GROUP_DENSITY;
+
+        public static DataMatrix GetRandomMatrix(int width, int height)
         {
-            DataMatrix matrix = new DataMatrix(width, height);
+            DataMatrix matrix = new DataMatrix((short)width, (short)height);
 
             Random rand = new Random();
 
             for (int i = 0; i < width; i++ )
             {
-                for (int j = 0; j < width; j++)
+                for (int j = 0; j < height; j++)
                 {
-                    matrix[i, j] = new Element(GetRandomElement(rand), false);
+                    if (i == 0 && j == 0)
+                    {
+                        matrix[i, j] = new Element(GetRandomElement(rand), false);
+                    }
+                    else if (i == 0)
+                    {
+                        matrix[i, j] = new Element(GetRandomElementWithGrouping(rand, matrix[i, (j - 1)], matrix[i, (j - 1)]), false);
+                    }
+                    else if (j == 0)
+                    {
+                        matrix[i, j] = new Element(GetRandomElementWithGrouping(rand, matrix[(i - 1), j], matrix[(i - 1), j]), false);
+                    }
+                    else
+                    {
+                        matrix[i, j] = new Element(GetRandomElementWithGrouping(rand, matrix[(i - 1), j], matrix[i, (j - 1)]), false);
+                    }
                 }
             }
             //TODO add testing for uniqueness of solution
             return matrix;
         }
 
-        private static Element.ElementType GetRandomElement(Random rand)
+        private static Element.ElementType GetRandomElement(Random rand, int density = BASIC_DENSITY)
         {
-            int lowthres = 0;
-            int hitres = 10;
-            int density = 6;
-
-            if (rand.Next(lowthres, hitres) < density)
+            int randInt = rand.Next(LOW_THRESHOLD, HIGH_THRESHOLD);
+            if (randInt > density)
             {
                 return Element.ElementType.Cleared;
             }
@@ -38,6 +62,27 @@ namespace BlueboxBack.Utilities
             {
                 return Element.ElementType.Filled;
             }
+        }
+        private static Element.ElementType GetRandomElementWithGrouping(Random rand, Element leftEl, Element topEl)
+        {
+            int density = BASIC_DENSITY;
+            if(leftEl.Type == Element.ElementType.Cleared && topEl.Type == Element.ElementType.Cleared)
+            {
+                density = space_density;
+                space_density += DENSITY_CHANGE_FACTOR;
+            }
+            else if (leftEl.Type == Element.ElementType.Filled && topEl.Type == Element.ElementType.Filled)
+            {
+                density = group_density;
+                group_density -= DENSITY_CHANGE_FACTOR;
+            }
+            else
+            {
+                space_density -= DENSITY_CHANGE_FACTOR;
+                group_density += DENSITY_CHANGE_FACTOR;
+            }
+
+            return GetRandomElement(rand, density);
         }
     }
 }
