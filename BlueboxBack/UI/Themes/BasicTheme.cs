4556fb20c9dbb4932ea7c43671e045adea00272e
Dmitriy Balykin <b_dmitriy82@mail.ru>
2015-01-29 11:03:23 +0200

    Change basic theme colors

--- BlueboxBack/UI/Themes/BasicTheme.cs
+++ BlueboxBack/UI/Themes/BasicTheme.cs
@@ -10,8 +10,8 @@ namespace BlueboxBack.UI.Themes
         public static Brush UndefinedBrush = new SolidBrush(Color.FromArgb(0xc0, 0xc0, 0xc0));
         public static Brush FilledBrush = Brushes.Blue;
         public static Brush ClearedBrush = Brushes.White;
-        public static Brush HighlightedFilledBrush = Brushes.LightCyan;
-        public static Brush HighlightedClearedBrush = Brushes.LightPink;
+        public static Brush HighlightedFilledBrush = Brushes.DarkCyan;
+        public static Brush HighlightedClearedBrush = new SolidBrush(Color.FromArgb(0xff, 0xea, 0xaa));
         public static Brush HeaderBackground = Brushes.LightGray;
         public static Brush HeaderForeground = Brushes.Black;
         public static Pen GridPen = new Pen(Color.FromArgb(0xf0, 0xf0, 0xf0));
