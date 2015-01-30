7d33feff08c6f810cf1f514507bb03287a3da2ca
Dmitriy Balykin <b_dmitriy82@mail.ru>
2015-01-30 16:03:46 +0200

    Remove unused code

--- BlueboxBack/Core/DataHandler.cs
+++ BlueboxBack/Core/DataHandler.cs
@@ -17,15 +17,6 @@ namespace BlueboxBack.Core
         }
         public void UpdateData(BoxTypes boxType, int x, int y, ActionTypes actionTypes)
         { 
-            switch(boxType)
-            {
-                case BoxTypes.Grid:
-                    break;
-                case BoxTypes.HeaderHorizontal:
-                    break;
-                case BoxTypes.HeaderVertical:
-                    break;
-            }
             DataUpdated(this, new DataUpdatedEventArgs() {
                 Data = manager.CalculateMatrix(boxType, actionTypes, x, y),
                 Solution = manager.GetSolutionMatrix()
