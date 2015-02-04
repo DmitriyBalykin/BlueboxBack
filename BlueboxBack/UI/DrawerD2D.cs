3e2428bdcb8b9e1588370fdd49370bbf0c6e66b6
Dmitriy Balykin <b_dmitriy82@mail.ru>
2015-02-04 15:50:25 +0200

    Add D2D drawing libraries

--- BlueboxBack/UI/DrawerD2D.cs
+++ BlueboxBack/UI/DrawerD2D.cs
@@ -6,14 +6,19 @@ using BlueboxBack.UI.Themes;
 using BlueboxBack.Utilities;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Managed.Graphics.Direct2D;
+using Managed.Graphics.DirectWrite;
+using Managed.Graphics.Imaging;
 
 namespace BlueboxBack.UI
 {
-    class Drawer
+    class DrawerD2D
     {
+        private Direct2DFactory factory;
+        private WindowRenderTarget rt;
+
         private static Brush UndefinedBrush = BasicTheme.UndefinedBrush;
         private static Brush FilledBrush = BasicTheme.FilledBrushDefault;
         private static Brush ClearedBrush = BasicTheme.ClearedBrush;
@@ -21,12 +26,13 @@ namespace BlueboxBack.UI
         private static Brush HighlightedClearedBrush = BasicTheme.HighlightedClearedBrush;
         private static Brush HeaderBackground = BasicTheme.HeaderBackground;
         private static Brush HeaderForeground = BasicTheme.HeaderForeground;
+        private static Brush HeaderIndexForeground = BasicTheme.HeaderIndexForeground;
         private static Pen GridPen = BasicTheme.GridPen;
         private static Pen GridPenThick = BasicTheme.GridPenThich;
         private static Brush HeaderFontBrush = BasicTheme.HeaderBrush;
         private static Brush HeaderFontBrushHighlighted = BasicTheme.HeaderBrushHighlighted;
         private static Font HeaderFont = BasicTheme.HeaderFont;
-        public static void Draw(Box pictureBox, DataMatrix dataMatrix, DataMatrix solutionMatrix)
+        public void Draw(Box pictureBox, DataMatrix dataMatrix, DataMatrix solutionMatrix)
         {
             int imageWidth = 0;
             int imageHeight = 0;
@@ -47,11 +53,6 @@ namespace BlueboxBack.UI
                     break;
             }
 
-            Bitmap drawArea = new Bitmap(imageWidth, imageHeight);
-            pictureBox.Image = drawArea;
-
-            Graphics g = Graphics.FromImage(drawArea);
-
             switch (pictureBox.type)
             {
                 case BoxTypes.Grid:
@@ -64,11 +65,8 @@ namespace BlueboxBack.UI
                     DrawVerticalHeader(g, dataMatrix, solutionMatrix);
                     break;
             }
-
-            pictureBox.Image = drawArea;
-            g.Dispose();
         }
-        public static void Draw(Box pictureBox, DataMatrix dataMatrix, DataMatrix solutionMatrix, ResultEvent.ResultType result)
+        public void Draw(Box pictureBox, DataMatrix dataMatrix, DataMatrix solutionMatrix, ResultEvent.ResultType result)
         {
             switch (result)
             {
@@ -89,9 +87,8 @@ namespace BlueboxBack.UI
             FilledBrush = BasicTheme.FilledBrushDefault;
         }
 
-        private static void DrawVerticalHeader(Graphics g, DataMatrix dataMatrix, DataMatrix solutionMatrix)
+        private void DrawVerticalHeader(DataMatrix dataMatrix, DataMatrix solutionMatrix)
         {
-            g.FillRectangle(HeaderBackground, g.ClipBounds);
 
             for (short i = 0; i < solutionMatrix.Height; i++)
             {
@@ -103,20 +100,18 @@ namespace BlueboxBack.UI
                     Brush brush = null;
                     Font font = null;
                     GetHeaderStyle(i, null, block, dataMatrix, out brush, out font);
-                    g.DrawString(String.Format("{0,2}", block.Length), font, brush, point);
+                    DrawString(String.Format("{0,2}", block.Length), font, brush, point);
                 }
-                g.DrawString((i+1).ToString(), HeaderFont, Brushes.Blue, new PointF(3, i * Constants.CELL_SIDE + 2));
-                g.DrawLine(GetGridPen(i), 0, i * Constants.CELL_SIDE, Constants.HEADER_SIZING, i * Constants.CELL_SIDE);
+                DrawString((i + 1).ToString(), HeaderFont, HeaderIndexForeground, new PointF(3, i * Constants.CELL_SIDE + 2));
+                DrawLine(GetGridPen(i), 0, i * Constants.CELL_SIDE, Constants.HEADER_SIZING, i * Constants.CELL_SIDE);
             }
         }
 
-        private static void DrawHorizontalHeader(Graphics g, DataMatrix dataMatrix, DataMatrix solutionMatrix)
+        private void DrawHorizontalHeader(DataMatrix dataMatrix, DataMatrix solutionMatrix)
         {
-            g.FillRectangle(HeaderBackground, g.ClipBounds);
-
             for (short i = 0; i < solutionMatrix.Width; i++)
             {
-                short j = Constants.HEADER_SIZING/20;
+                short j = Constants.HEADER_SIZING / 20;
                 foreach (CellsBlock block in solutionMatrix.GetCountersListReversed(null, i))
                 {
                     PointF point = new PointF(i * Constants.CELL_SIDE, (j - 1) * 20);
@@ -124,24 +119,24 @@ namespace BlueboxBack.UI
                     Brush brush = null;
                     Font font = null;
                     GetHeaderStyle(null, i, block, dataMatrix, out brush, out font);
-                    g.DrawString(String.Format("{0,2}", block.Length), font, brush, point);
+                    DrawString(String.Format("{0,2}", block.Length), font, brush, point);
                 }
-                g.DrawString((i+1).ToString(), HeaderFont, Brushes.Blue, new PointF(i * Constants.CELL_SIDE, 3));
-                g.DrawLine(GetGridPen(i), i * Constants.CELL_SIDE, 0, i * Constants.CELL_SIDE, Constants.HEADER_SIZING);
+                DrawString((i + 1).ToString(), HeaderFont, HeaderIndexForeground, new PointF(i * Constants.CELL_SIDE, 3));
+                DrawLine(GetGridPen(i), i * Constants.CELL_SIDE, 0, i * Constants.CELL_SIDE, Constants.HEADER_SIZING);
             }
         }
 
-        private static void DrawGrid(Graphics g, DataMatrix matrix)
+        private void DrawGrid(DataMatrix matrix)
         {
             int side = Constants.CELL_SIDE;
             bool horizLinesDrawn = false;
 
             //draw cells
-            for(int i = 0; i < matrix.Width; i++)
+            for (int i = 0; i < matrix.Width; i++)
             {
-                for(int j = 0; j < matrix.Height; j++)
+                for (int j = 0; j < matrix.Height; j++)
                 {
-                    g.FillRectangle(GetCellBrush(matrix[i, j]), i * side, j * side, (i + 1) * side, (j + 1) * side);
+                    DrawRectangle(new PointF(i * side, j * side), new PointF((i + 1) * side, (j + 1) * side), GetCellBrush(matrix[i, j]));
                 }
             }
             //draw grid
@@ -151,17 +146,17 @@ namespace BlueboxBack.UI
                 {
                     if (!horizLinesDrawn)
                     {
-                        g.DrawLine(GetGridPen(j), 0, j * side, matrix.Width * side, j * side); //horizontal line
+                        DrawLine(GetGridPen(j), new PointF(0, j * side), new PointF(matrix.Width * side, j * side)); //horizontal line
                     }
                 }
                 horizLinesDrawn = true;
-                g.DrawLine(GetGridPen(i), i * side, 0, i * side, matrix.Height * side); //vertical line
+                DrawLine(GetGridPen(i), new PointF(i * side, 0), new PointF(i * side, matrix.Height * side)); //vertical line
             }
         }
 
         private static Brush GetCellBrush(Element element)
         {
-            switch(element.Type)
+            switch (element.Type)
             {
                 case Element.ElementType.Cleared:
                     return element.Highlighted ? HighlightedClearedBrush : ClearedBrush;
@@ -178,7 +173,7 @@ namespace BlueboxBack.UI
         }
         private static Pen GetGridPen(int i)
         {
-            if(i % Constants.THICK_GRID_STEP == 0)
+            if (i % Constants.THICK_GRID_STEP == 0)
             {
                 return GridPenThick;
             }
@@ -200,5 +195,12 @@ namespace BlueboxBack.UI
                 font = BasicTheme.HeaderFont;
             }
         }
+
+        private void DrawLine(PointF point1, PointF point2, Brush brush)
+        { }
+        private void DrawRectangle(PointF point1, PointF point2, Brush brush)
+        { }
+        private void DrawString(string str, Font font, Brush brush, PointF point)
+        { }
     }
 }
