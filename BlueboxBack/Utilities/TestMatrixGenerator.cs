88dd4c61a962557b6fc0d594ad45e60416224e23
Dmitriy Balykin <b_dmitriy82@mail.ru>
2015-02-05 17:07:10 +0200

    Add test matrix generator

---
+++ BlueboxBack/Utilities/TestMatrixGenerator.cs
@@ -0,0 +1,27 @@
+﻿using BlueboxBack.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlueboxBack.Utilities
+{
+    class TestMatrixGenerator
+    {
+        public static DataMatrix GetMatrix(int width, int height)
+        {
+            DataMatrix result = GetTwoVariantsSolutionMatrix(width, height);
+
+            return result;
+        }
+        private static DataMatrix GetTwoVariantsSolutionMatrix(int width, int height)
+        {
+            DataMatrix result = new DataMatrix(width, height, Element.ElementType.Cleared);
+            result[0, 0] = new Element(Element.ElementType.Filled);
+            result[0, 1] = new Element(Element.ElementType.Filled);
+            result[1, 1] = new Element(Element.ElementType.Filled);
+            result[1, 2] = new Element(Element.ElementType.Filled);
+
+            return result;
+        }
+    }
+}
